79c8b46378dcc7a693801db86e8567edd8c26dca
Denis ING <denis_ge@yahoo.fr>
2014-11-17 14:35:39 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TKArchitecture::Application.config.secret_key_base = '8e103ff331732bfe9e90c1bbc7231a3a2937b4027f6644489a1247738f13a2e11cf48ffa237e26fcb20f51777591f3f5bcc99bcccce2506d4a2ac36abe8ef7c9'
