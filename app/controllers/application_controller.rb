4ded2002f7661633958dbac17550a279094797df
Denis ING <denis_ge@yahoo.fr>
2014-11-18 07:20:41 +0100

    Default Config i18n french

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_user_language
+  #before_action :set_user_language
 
-  private
+  #private
 
-  def set_user_language
-    I18n.locale = 'fr'
-  end
+  #def set_user_language
+  #  I18n.locale = 'fr'
+  #end
  
 end
