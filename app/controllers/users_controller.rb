2a7f9b26ee0108d4237d538fe8905dc2e39e26ab
Denis ING <denis_ge@yahoo.fr>
2014-11-27 08:30:45 +0100

    Correction link internazionalisation with Before Filter, but problem with arabic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_filter :set_locale
+
   def new
     @user = User.new
   end
@@ -8,7 +11,12 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to root_url, notice: "Signed up!"
     else
+      flash[:notice] = "Invalid !!!!"
       render :new
     end
+  end
+
+  def set_locale
+    I18n.locale =  params[:locale]
   end
 end
